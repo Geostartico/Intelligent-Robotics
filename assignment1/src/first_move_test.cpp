d82ac12e086e53f9a8be3f11f67e04eda07e036d
Giovanni Artico <AD\artigio86863@DEI-INT-ROB-002.vdi.ict.unipd.it>
2024-11-24 11:09:46 +0100

    tests for map

--- assignment1/src/first_move_test.cpp
+++ assignment1/src/first_move_test.cpp
@@ -4,20 +4,22 @@
 
 int main(int argc, char **argv)
 {
+	ros::init(argc, argv, "first_move_test");
     actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> moveBase("move_base", true);
     moveBase.waitForServer();
 
     move_base_msgs::MoveBaseGoal goal;
-    goal.target_pose.pose.position.x = 1;
-    goal.target_pose.pose.position.y = 1;
+    goal.target_pose.header.seq =1;
+    goal.target_pose.pose.position.x = 11.7;
+    goal.target_pose.pose.position.y = -3.3;
     goal.target_pose.pose.position.z = 0.0;
     goal.target_pose.header.stamp = ros::Time::now();
     goal.target_pose.header.frame_id = "map";
 
-    goal.target_pose.pose.orientation.x = 0;
-    goal.target_pose.pose.orientation.y = 0;
-    goal.target_pose.pose.orientation.z = 0;
-    goal.target_pose.pose.orientation.w = 0;
+    goal.target_pose.pose.orientation.x = 0.0;
+    goal.target_pose.pose.orientation.y = 0.0;
+    goal.target_pose.pose.orientation.z = 0.0;
+    goal.target_pose.pose.orientation.w = 1.0;
 
     // Send goal.
     moveBase.sendGoal(goal);
