f1eb0aaf82217239859408f4f8e3c0ee981adb55
trigger <francesco.colla.2@studenti.unipd.it>
2024-12-01 12:17:23 +0100

    blackroi 1st attempt

--- assignment1/src/movement_handler.cpp
+++ assignment1/src/movement_handler.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <tf/transform_listener.h>
 #include <actionlib/server/simple_action_server.h>
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
@@ -8,25 +9,32 @@
 
 typedef actionlib::SimpleActionServer<assignment1::WaypointMoveAction> Server;
 
+struct blackROI{
+    double xl;
+    double xm;
+    double yl;
+    double ym;
+};
+
 class MovementHandler
 {
     protected:
     ros::NodeHandle nh_;
-    Server as_;  //ActionServer
-    std::string action_name_;
-    assignment1::WaypointMoveFeedback feedback_;
-    assignment1::WaypointMoveResult result_;
-    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> move_base_client_;
+    Server as;  //ActionServer
+    std::string action_name;
+    assignment1::WaypointMoveFeedback feedback;
+    assignment1::WaypointMoveResult result;
+    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> move_base_client;
 
 
     public:
 
         MovementHandler(std::string name) :
-            as_(nh_, name, boost::bind(&MovementHandler::callbackOnGoal, this, _1), false),
-            action_name_(name),
-            move_base_client_("move_base", true)
+            as(nh_, name, boost::bind(&MovementHandler::callbackOnGoal, this, _1), false),
+            action_name(name),
+            move_base_client("move_base", true)
             {
-                as_.start();
+                as.start();
             }
     
         void callbackOnGoal(const assignment1::WaypointMoveGoal::ConstPtr &goal)
@@ -44,26 +52,72 @@ class MovementHandler
             move_goal.target_pose.pose.orientation.z = 0.0;
             move_goal.target_pose.pose.orientation.w = 1.0;
 
-            move_base_client_.sendGoal(move_goal);
-            bool timeout = move_base_client_.waitForResult(ros::Duration(45.0));
-            if(timeout)
+            move_base_client.sendGoal(move_goal);
+
+            ros::Time start_time = ros::Time::now();
+            ros::Duration timeout(20);
+
+            blackROI  fuckin_table{-0.267452, 1.732548, -1.918757, 0.081243};
+            
+            ros::Rate rate(10.0);
+            while (ros::ok())
             {
-                if(move_base_client_.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+                if ((ros::Time::now() - start_time) >  timeout)
                 {
-                    result_.reached=true;
-                    ROS_INFO("NAV OK");
-                } else
-                {
-                    result_.reached = false;
-                    ROS_ERROR("NAV FAILURE");
+                    ROS_ERROR("Navigation timed out - Cancelling goal...");
+                    move_base_client.cancelGoal();
+                    result.reached= false;
+                    as.setAborted(result, "Timeout abort");
+                    return;
                 }
-            }else
-            {
-                result_.reached = false;
-                ROS_ERROR("NAV TIMEOUT");
+                
+                
+                if(inBlackROI(goal->x, goal->y, fuckin_table)){
+                    move_base_client.cancelGoal();
+                    result.reached = false;
+                    as.setAborted(result, "BlackROI abort");
+                    return;
+                }
+                
+
+                if(move_base_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+                {
+                    result.reached=true;
+                    ROS_INFO("NAV OK");
+                    as.setSucceeded(result);
+                    return;
+                }else if(move_base_client.getState() == actionlib::SimpleClientGoalState::ABORTED)
+                {
+                    result.reached = false;
+                    ROS_ERROR("NAV FAIL");
+                    as.setAborted(result);
+                    return;
+                }
+                rate.sleep();
             }
-            as_.setSucceeded(result_);
         }
+
+    private:
+    bool inBlackROI(double x_goal, double y_goal, blackROI& ROI)
+    {
+        tf::StampedTransform transform;
+
+        double x_robot = transform.getOrigin().x();
+        double y_robot = transform.getOrigin().y();
+
+        if(x_goal>= ROI.xl && x_goal<= ROI.xm && y_goal >= ROI.yl, y_goal <= ROI.ym)
+        {
+            ROS_WARN("Waypoint is in a BlackROI, movement aborted");
+            return true;
+        } else if(x_robot >= ROI.xl && x_robot <= ROI.xm && y_robot >= ROI.yl && y_robot <= ROI.ym)
+        {
+            ROS_WARN("Robot entered a BlackROI, movement aborted");
+            return true;
+        }  else
+        {
+            return false;
+        }
+    }
 };
 
 int main(int argc, char** argv){
