8788559bfceae7ea7664910a170b0d003e4a1883
trigger <francesco.colla.2@studenti.unipd.it>
2024-12-07 11:41:20 +0100

    black roi starts to be OK

--- assignment1/src/movement_handler.cpp
+++ assignment1/src/movement_handler.cpp
@@ -10,10 +10,15 @@
 typedef actionlib::SimpleActionServer<assignment1::WaypointMoveAction> Server;
 
 struct blackROI{
-    double xl;
-    double xm;
-    double yl;
-    double ym;
+    double x_lower;
+    double x_max;
+    double y_lower;
+    double y_max;
+};
+
+struct  robotPos{
+    double x_robot;
+    double y_robot;
 };
 
 class MovementHandler
@@ -56,12 +61,25 @@ class MovementHandler
             ros::Time start_time = ros::Time::now();
             ros::Duration timeout(20);
 
-            blackROI  fuckin_table{-0.267452, 1.732548, -1.918757, 0.081243};
-            // blackROI  fuckin_table{100.267452, 101.732548, 100.918757, 103.081243};
+            blackROI  fuckin_table{6.78863,7.78863,-1.82448,-2.82448};
+            
+            move_base_msgs::MoveBaseGoal  move_emergency_goal;
+            move_emergency_goal.target_pose.header.frame_id = "map";
+            move_emergency_goal.target_pose.pose.position.x = fuckin_table.x_max;
+            move_emergency_goal.target_pose.pose.position.y = fuckin_table.y_max;
+            move_emergency_goal.target_pose.pose.position.z = 0.0;
+            move_emergency_goal.target_pose.header.stamp = ros::Time::now();
+
+            move_emergency_goal.target_pose.pose.orientation.x = 0.0;
+            move_emergency_goal.target_pose.pose.orientation.y = 0.0;
+            move_emergency_goal.target_pose.pose.orientation.z = 0.0;
+            move_emergency_goal.target_pose.pose.orientation.w = 1.0;
             
             ros::Rate rate(10.0);
             while (ros::ok())
             {
+                robotPos currentPos = currentRobotPos();
+                
                 if ((ros::Time::now() - start_time) >  timeout)
                 {
                     ROS_ERROR("Navigation timed out - Cancelling goal...");
@@ -71,13 +89,25 @@ class MovementHandler
                     return;
                 }
                 
-                
-                if(inBlackROI(goal->x, goal->y, fuckin_table)){
-                    move_base_client.cancelGoal();
-                    result.reached = false;
-                    as.setAborted(result, "BlackROI abort");
-                    return;
+                char blackROIcheck = inBlackROI(goal->x, goal->y, currentPos.x_robot, currentPos.y_robot, fuckin_table);
+                if (blackROIcheck!=0)
+                {
+                    if(blackROIcheck ==  1)
+                    {
+                        ROS_WARN("Waypoint generated inside black ROI, deleting...");
+                        move_base_client.cancelGoal();
+                        result.reached = false;
+                        as.setAborted(result, "BlackROI abort");
+                        return;
+                    }else if (blackROIcheck == 2)
+                    {
+                        ROS_WARN("Movement in blackROI, redirecting...");
+                        move_base_client.cancelGoal();
+                        move_base_client.sendGoal(move_emergency_goal);
+                        move_base_client.sendGoal(move_goal);
+                    }
                 }
+                
                 
 
                 if(move_base_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
@@ -98,31 +128,40 @@ class MovementHandler
         }
 
     private:
-    bool inBlackROI(double x_goal, double y_goal, blackROI& ROI)
+
+    robotPos currentRobotPos()
     {
         tf::StampedTransform transform;
         tf::TransformListener listener;
         listener.waitForTransform("map", "base_link", ros::Time(0), ros::Duration(3.0));
         listener.lookupTransform("map", "base_link", ros::Time(0), transform);
 
-        double x_robot = transform.getOrigin().x();
-        double y_robot = transform.getOrigin().y();
+        double x = transform.getOrigin().x();
+        double y = transform.getOrigin().y();
 
-        ROS_INFO("BLACKROI coord: xl:%f xm:%f yl:%f ym:%f", ROI.xl, ROI.xm, ROI.yl, ROI.ym);
+        robotPos current {x,y};
+
+        return(current);
+    };
+
+    char inBlackROI(double x_goal, double y_goal, double x_robot, double y_robot,blackROI& ROI)
+    {
+
+        ROS_INFO("BLACKROI coord: xl:%f xm:%f yl:%f ym:%f", ROI.x_lower, ROI.x_max, ROI.y_lower, ROI.y_max);
         ROS_INFO("GOAL coord:      x:%f  y:%f", x_goal, y_goal);
         ROS_INFO("ROBOT coord :    x:%f  y:%f", x_robot, y_robot);
 
-        if(x_goal>= ROI.xl && x_goal<= ROI.xm && y_goal >= ROI.yl && y_goal <= ROI.ym)
+        if(x_goal>= ROI.x_lower && x_goal<= ROI.x_max && y_goal >= ROI.y_max && y_goal <= ROI.y_lower)
         {
             ROS_WARN("Waypoint is in a BlackROI, movement aborted");
-            return true;
-        } else if(x_robot >= ROI.xl && x_robot <= ROI.xm && y_robot >= ROI.yl && y_robot <= ROI.ym)
+            return 1;
+        } else if(x_robot >= ROI.x_lower && x_robot <= ROI.x_max && y_robot >= ROI.y_max && y_robot <= ROI.y_lower)
         {
-            ROS_WARN("Robot entered a BlackROI, movement aborted");
-            return true;
+            ROS_WARN("Robot entered a BlackROI, movement redirected");
+            return 2;
         }  else
         {
-            return false;
+            return 0;
         }
     }
 };
