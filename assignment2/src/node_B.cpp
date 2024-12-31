7c70c92eebcc1e5df4ff6201637fdca14606a4b2
giovanni artico <giovanni.artico@gmail.com>
2024-12-31 16:22:40 +0100

    new routine

--- assignment2/src/node_B.cpp
+++ assignment2/src/node_B.cpp
@@ -44,6 +44,10 @@ const geometry_msgs::Quaternion NEG_Y_ORIENTATION = [] {
     return q;
 }();
 
+int curpos= 1;
+std::vector<std::pair<float, float>> docks, corns;
+
+
 void sendGoalToMoveBase(double x, double y, const geometry_msgs::Quaternion& orientation) {
 
     MoveBaseClient ac("move_base", true);
@@ -69,6 +73,74 @@ void sendGoalToMoveBase(double x, double y, const geometry_msgs::Quaternion& ori
         ROS_WARN("Failed to reach the goal.");
     }
 }
+void go_clockwise(int target_pos){
+    //main objective:survive
+    geometry_msgs::Quaternion escape_orientation;
+    if(curpos==2){
+        escape_orientation = POS_X_ORIENTATION;
+    }
+    if(curpos==3 || curpos==4){
+        escape_orientation = NEG_Y_ORIENTATION;
+    }
+    if(curpos==6 || curpos==1){
+        escape_orientation = POS_Y_ORIENTATION;
+    }
+    sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, escape_orientation);
+
+    if(curpos==1&&target_pos>curpos){
+        sendGoalToMoveBase(corns[0].first, corns[0].second, POS_Y_ORIENTATION);
+        sendGoalToMoveBase(corns[0].first, corns[0].second, POS_X_ORIENTATION);
+        curpos++;
+        sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, POS_X_ORIENTATION);
+        if(target_pos==2){
+            sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, NEG_Y_ORIENTATION);
+            return;
+        }
+    }
+    if(curpos==2&&target_pos>curpos){
+        sendGoalToMoveBase(corns[1].first, corns[1].second, POS_X_ORIENTATION);
+        sendGoalToMoveBase(corns[1].first, corns[1].second, NEG_Y_ORIENTATION);
+        curpos++;
+        sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, NEG_Y_ORIENTATION);
+        if(target_pos==3){
+            sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, NEG_X_ORIENTATION);
+            return;
+        }
+    }
+    if(curpos==3&&target_pos>curpos){
+        curpos++;
+        sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, NEG_Y_ORIENTATION);
+        if(target_pos==4){
+            sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, NEG_X_ORIENTATION);
+            return;
+        }
+    }
+    if(curpos==4&&target_pos>curpos){
+        sendGoalToMoveBase(corns[2].first, corns[2].second, NEG_Y_ORIENTATION);
+        sendGoalToMoveBase(corns[2].first, corns[2].second, NEG_X_ORIENTATION);
+        curpos++;
+        sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, NEG_X_ORIENTATION);
+        if(target_pos==5){
+            sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, POS_Y_ORIENTATION);
+            return;
+        }
+    }
+    if(curpos==5&&target_pos>curpos){
+        sendGoalToMoveBase(corns[3].first, corns[3].second, NEG_X_ORIENTATION);
+        sendGoalToMoveBase(corns[3].first, corns[3].second, POS_Y_ORIENTATION);
+        curpos++;
+        sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, POS_Y_ORIENTATION);
+        if(target_pos==6){
+            sendGoalToMoveBase(docks[curpos-1].first, docks[curpos-1].second, POS_X_ORIENTATION);
+            return;
+        }
+    }
+}
+void goAround(int target_pos){
+    if(target_pos >curpos){
+        go_clockwise(target_pos);
+    }
+}
 
 bool detection_routine(assignment2::object_detect::Request &req, assignment2::object_detect::Response &res)
 {
@@ -85,15 +157,18 @@ bool detection_routine(assignment2::object_detect::Request &req, assignment2::ob
         std::make_pair(DIST, 0)
     };
     std::vector<std::pair<float, float>> corns_offsets = {
-        std::make_pair(-DIST, -DIST), 
-        std::make_pair(DIST, -DIST)
+        std::make_pair(-DIST, DIST),
+        std::make_pair(DIST, DIST) 
     };
 
-    std::vector<std::pair<float, float>> docks, corns;
     for(auto off : docks_offsets) 
-        docks.push_back(std::make_pair(TABLE_2_X + off.first, TABLE_2_Y + off.second));
+        docks.push_back(std::make_pair(TABLE_1_X + off.first, TABLE_1_Y - off.second));
+    for(auto off : docks_offsets) 
+        docks.push_back(std::make_pair(TABLE_2_X - off.first, TABLE_2_Y + off.second));
     for(auto off : corns_offsets) 
-        corns.push_back(std::make_pair(TABLE_2_X + off.first, TABLE_2_Y + off.second));
+        corns.push_back(std::make_pair(TABLE_1_X + off.first, TABLE_1_Y + off.second));
+    for(auto off : corns_offsets) 
+        corns.push_back(std::make_pair(TABLE_2_X - off.first, TABLE_2_Y - off.second));
 
     ROS_INFO("Docking positions:");
     for(auto p : docks)
@@ -105,28 +180,30 @@ bool detection_routine(assignment2::object_detect::Request &req, assignment2::ob
     std::vector<float> objs_x, objs_y, docks_x, docks_y;
     std::vector<int> ids;
 
-    // Intermediate point
-    sendGoalToMoveBase(docks[0].first, 0.0, NEG_Y_ORIENTATION);
+    sendGoalToMoveBase(docks[0].first, 0.0, POS_X_ORIENTATION);
+    goAround(6);
+    //// Intermediate point
+    //sendGoalToMoveBase(docks[0].first, 0.0, NEG_Y_ORIENTATION);
 
-    // First table side
-    sendGoalToMoveBase(docks[0].first, docks[0].second, POS_X_ORIENTATION);
-    // DETECTION HERE
-    sendGoalToMoveBase(docks[0].first, docks[0].second, NEG_Y_ORIENTATION);
+    //// First table side
+    //sendGoalToMoveBase(docks[0].first, docks[0].second, POS_X_ORIENTATION);
+    //// DETECTION HERE
+    //sendGoalToMoveBase(docks[0].first, docks[0].second, NEG_Y_ORIENTATION);
 
-    // First corner
-    sendGoalToMoveBase(corns[0].first, corns[0].second, POS_X_ORIENTATION);   
+    //// First corner
+    //sendGoalToMoveBase(corns[0].first, corns[0].second, POS_X_ORIENTATION);   
 
-    // Second table side
-    sendGoalToMoveBase(docks[1].first, docks[1].second, POS_Y_ORIENTATION);
-    // DETECTION HERE
-    sendGoalToMoveBase(docks[1].first, docks[1].second, POS_X_ORIENTATION); 
+    //// Second table side
+    //sendGoalToMoveBase(docks[1].first, docks[1].second, POS_Y_ORIENTATION);
+    //// DETECTION HERE
+    //sendGoalToMoveBase(docks[1].first, docks[1].second, POS_X_ORIENTATION); 
 
-    // Second corner
-    sendGoalToMoveBase(corns[1].first, corns[1].second, POS_Y_ORIENTATION);   
+    //// Second corner
+    //sendGoalToMoveBase(corns[1].first, corns[1].second, POS_Y_ORIENTATION);   
 
-    // Third table side
-    sendGoalToMoveBase(docks[2].first, docks[2].second, NEG_X_ORIENTATION);
-    // DETECTION HERE
+    //// Third table side
+    //sendGoalToMoveBase(docks[2].first, docks[2].second, NEG_X_ORIENTATION);
+    //// DETECTION HERE
 
     res.objs_x = objs_x;
     res.objs_y = objs_y;
