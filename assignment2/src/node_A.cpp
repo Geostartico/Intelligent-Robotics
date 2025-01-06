4a207d1fc474546d9d1ab666bed72863378f5276
Mattia Toffolon <AD\toffmat63436@DEI-INT-ROB-001.vdi.ict.unipd.it>
2025-01-06 13:24:16 +0100

    letsgoski

--- assignment2/src/node_A.cpp
+++ assignment2/src/node_A.cpp
@@ -58,80 +58,79 @@ void add_reference_collisions(float start_x, float start_y, float m, float q){
     planning_scene_interface.applyCollisionObjects(collision_objects);
 
 }
-void put_down_routine(std::vector<apriltag_str> tags, apriltag_str table_tag, int& put_objs, float m, float q, Movement& mov) {
-    for (const auto& tag:tags) {
-        actionlib::SimpleActionClient<assignment2::ObjectMoveAction> ac("move_object", true);
-        ROS_INFO("Waiting for move_object server to start.");
-        ac.waitForServer();
 
-        // PICK UP
-        ROS_INFO("Picking up object with AprilTag %u at x=%f y=%f from dock %u.", tag.id, tag.x, tag.y, tag.dock);
-        mov.goAround(tag.dock);
+void put_down_routine(apriltag_str tag, apriltag_str table_tag, int& put_objs, float m, float q, Movement& mov) {
+    actionlib::SimpleActionClient<assignment2::ObjectMoveAction> ac("move_object", true);
+    ROS_INFO("Waiting for move_object server to start.");
+    ac.waitForServer();
 
-        assignment2::ObjectMoveGoal goal_pick;
-        goal_pick.pick = true;
-        goal_pick.tgt_id = tag.id;
-        goal_pick.tgt_pose.position.x =  tag.x;
-        goal_pick.tgt_pose.position.y =  tag.y;
-        goal_pick.tgt_pose.position.z =  tag.z;
+    // PICK UP
+    ROS_INFO("Picking up object with AprilTag %u at x=%f y=%f from dock %u.", tag.id, tag.x, tag.y, tag.dock);
+    mov.goAround(tag.dock);
 
-        tf2::Quaternion qt;
-        qt.setRPY(0, M_PI_2, 0);
+    assignment2::ObjectMoveGoal goal_pick;
+    goal_pick.pick = true;
+    goal_pick.tgt_id = tag.id;
+    goal_pick.tgt_pose.position.x =  tag.x;
+    goal_pick.tgt_pose.position.y =  tag.y;
+    goal_pick.tgt_pose.position.z =  tag.z;
 
-        goal_pick.tgt_pose.orientation.w = qt.w();
-        goal_pick.tgt_pose.orientation.x = qt.x();
-        goal_pick.tgt_pose.orientation.y = qt.y();
-        goal_pick.tgt_pose.orientation.z = qt.z();
+    tf2::Quaternion qt;
+    qt.setRPY(0, M_PI_2, 0);
 
-        ROS_INFO("Sending goal to move_object server.");
-        ac.sendGoal(goal_pick);
-        bool finished_before_timeout = ac.waitForResult(ros::Duration(45.0));
-        if (finished_before_timeout) {
-            actionlib::SimpleClientGoalState state = ac.getState();
-            if (state == actionlib::SimpleClientGoalState::SUCCEEDED) {
-                ROS_INFO("Action succeeded!");
-            } else {
-                ROS_WARN("Action did not succeed. State: %s", state.toString().c_str());
-            }
+    goal_pick.tgt_pose.orientation.w = qt.w();
+    goal_pick.tgt_pose.orientation.x = qt.x();
+    goal_pick.tgt_pose.orientation.y = qt.y();
+    goal_pick.tgt_pose.orientation.z = qt.z();
+
+    ROS_INFO("Sending goal to move_object server.");
+    ac.sendGoal(goal_pick);
+    bool finished_before_timeout = ac.waitForResult(ros::Duration(45.0));
+    if (finished_before_timeout) {
+        actionlib::SimpleClientGoalState state = ac.getState();
+        if (state == actionlib::SimpleClientGoalState::SUCCEEDED) {
+            ROS_INFO("Action succeeded!");
+        } else {
+            ROS_WARN("Action did not succeed. State: %s", state.toString().c_str());
         }
-        else
-            ROS_WARN("Action did not finish before timeout.");
-	    
-        std::pair<float, float> coords = compute_coord(table_tag.x, table_tag.y, put_objs, m, q);
-        float put_down_y = coords.second;
-        float put_down_x = coords.first;
-
-        int closestDock = mov.closest_dock(put_down_x, put_down_y);
-
-        //PLACE DOWN
-        ROS_INFO("Placing down object with AprilTag %u in x=%f y=%f from dock %u", tag.id, put_down_x, put_down_y, closestDock);
-        mov.goAround(closestDock);
-        
-        assignment2::ObjectMoveGoal goal_place;
-        goal_place.pick = false;
-        goal_place.tgt_id = tag.id;
-        goal_place.tgt_pose.position.x =  put_down_x;
-        goal_place.tgt_pose.position.y =  put_down_y;
-        goal_place.tgt_pose.position.z =  tag.z;
-        goal_place.tgt_pose.orientation.w = qt.w();
-        goal_place.tgt_pose.orientation.x = qt.x();
-        goal_place.tgt_pose.orientation.y = qt.y();
-        goal_place.tgt_pose.orientation.z = qt.z();
-        ROS_INFO("Sending goal to move_object server.");
-        ac.sendGoal(goal_place);
-        finished_before_timeout = ac.waitForResult(ros::Duration(45.0));
-        if (finished_before_timeout) {
-            actionlib::SimpleClientGoalState state = ac.getState();
-            if (state == actionlib::SimpleClientGoalState::SUCCEEDED) {
-                ROS_INFO("Action succeeded!");
-            } else {
-                ROS_WARN("Action did not succeed. State: %s", state.toString().c_str());
-            }
-        }
-        else
-            ROS_WARN("Action did not finish before timeout.");
-	    put_objs++;
     }
+    else
+        ROS_WARN("Action did not finish before timeout.");
+    
+    std::pair<float, float> coords = compute_coord(table_tag.x, table_tag.y, put_objs, m, q);
+    float put_down_y = coords.second;
+    float put_down_x = coords.first;
+
+    int closestDock = mov.closest_dock(put_down_x, put_down_y);
+
+    //PLACE DOWN
+    ROS_INFO("Placing down object with AprilTag %u in x=%f y=%f from dock %u", tag.id, put_down_x, put_down_y, closestDock);
+    mov.goAround(closestDock);
+    
+    assignment2::ObjectMoveGoal goal_place;
+    goal_place.pick = false;
+    goal_place.tgt_id = tag.id;
+    goal_place.tgt_pose.position.x =  put_down_x;
+    goal_place.tgt_pose.position.y =  put_down_y;
+    goal_place.tgt_pose.position.z =  tag.z;
+    goal_place.tgt_pose.orientation.w = qt.w();
+    goal_place.tgt_pose.orientation.x = qt.x();
+    goal_place.tgt_pose.orientation.y = qt.y();
+    goal_place.tgt_pose.orientation.z = qt.z();
+    ROS_INFO("Sending goal to move_object server.");
+    ac.sendGoal(goal_place);
+    finished_before_timeout = ac.waitForResult(ros::Duration(45.0));
+    if (finished_before_timeout) {
+        actionlib::SimpleClientGoalState state = ac.getState();
+        if (state == actionlib::SimpleClientGoalState::SUCCEEDED) {
+            ROS_INFO("Action succeeded!");
+        } else {
+            ROS_WARN("Action did not succeed. State: %s", state.toString().c_str());
+        }
+    }
+    else
+        ROS_WARN("Action did not finish before timeout.");
+    put_objs++;
 }
 
 int main(int argc, char **argv) {
@@ -166,98 +165,103 @@ int main(int argc, char **argv) {
     std::vector<apriltag_str> dock4, dock5, dock6;
     std::map<int, apriltag_str> tags;
     apriltag_str table_tag{0,0,0,0,-1,-1};
+    apriltag_str to_move{0,0,0,0,-1,-1};
+    int put_objs = 0;
 
     for(int i=3; i<=4; i++) {
-        ROS_INFO("Moving to dock %u.", i);
-        mov.goAround(i);
-        // wait_time.sleep();
-
         std::vector<double> turns;
         if(i==3) turns = {0.0};
         else turns = {0.0, -M_PI_4/3, 2*M_PI_4/3};
 
-        for(auto turn: turns) {
-            mov.spin(turn);
-            assignment2::apriltag_detect ad_srv;
-            if(ad_client.call(ad_srv)) {
-                ROS_INFO("Call to apriltags_detected_service: SUCCESSFUL.");
-                std::vector<int> ids = ad_srv.response.ids;
-                std::vector<float> x = ad_srv.response.x;
-                std::vector<float> y = ad_srv.response.y;
-                std::vector<float> z = ad_srv.response.z;
-                std::vector<float> yaw = ad_srv.response.yaw;
-                ROS_INFO("Detections IDs: total=%lu, content=[%s]", 
-                        ids.size(),
-                        std::accumulate(ids.begin(), ids.end(), std::string(),
-                                        [](const std::string &a, int b) {
-                                            return a.empty() ? std::to_string(b) : a + ", " + std::to_string(b);
-                                        }).c_str());
-                for(int j=0; j<ids.size(); j++) {
-                    apriltag_str tmp{x[j], y[j], z[j], yaw[j], ids[j], i};
-                    if(ids[j]==10){
-                        if(table_tag.dock==-1)
-                            table_tag = tmp;
-                        continue;
-                    }
-                    auto it = tags.find(ids[j]);
-                    if(it != tags.end()) {
-                        if(mov.dock_dist(x[j], y[j], i) < mov.dock_dist(it->second.x, it->second.y, it->second.dock)) {
+        do {
+            ROS_INFO("Moving to dock %u.", i);
+            mov.goAround(i);
+            for(auto turn: turns) {
+                mov.spin(turn);
+                assignment2::apriltag_detect ad_srv;
+                if(ad_client.call(ad_srv)) {
+                    ROS_INFO("Call to apriltags_detected_service: SUCCESSFUL.");
+                    std::vector<int> ids = ad_srv.response.ids;
+                    std::vector<float> x = ad_srv.response.x;
+                    std::vector<float> y = ad_srv.response.y;
+                    std::vector<float> z = ad_srv.response.z;
+                    std::vector<float> yaw = ad_srv.response.yaw;
+                    ROS_INFO("Detections IDs: total=%lu, content=[%s]", 
+                            ids.size(),
+                            std::accumulate(ids.begin(), ids.end(), std::string(),
+                                            [](const std::string &a, int b) {
+                                                return a.empty() ? std::to_string(b) : a + ", " + std::to_string(b);
+                                            }).c_str());
+                    for(int j=0; j<ids.size(); j++) {
+                        apriltag_str tmp{x[j], y[j], z[j], yaw[j], ids[j], i};
+                        if(ids[j]==10){
+                            if(table_tag.dock==-1)
+                                table_tag = tmp;
+                            continue;
+                        }
+                        auto it = tags.find(ids[j]);
+                        if(it != tags.end()) {
+                            if(mov.dock_dist(x[j], y[j], i) < mov.dock_dist(it->second.x, it->second.y, it->second.dock)) {
+                                int closestDock = mov.closest_dock(x[j], y[j]);
+                                tmp.dock = closestDock;
+                                ROS_INFO("Updating AprilTag %u: from dock %u to dock %u", ids[j], it->second.dock, closestDock);
+                                tags[ids[j]] = tmp;
+                            }
+                        }
+                        else {
                             int closestDock = mov.closest_dock(x[j], y[j]);
                             tmp.dock = closestDock;
-                            ROS_INFO("Updating AprilTag %u: from dock %u to dock %u", ids[j], it->second.dock, closestDock);
+                            ROS_INFO("Adding new AprilTag %u at dock %u (x=%.2f, y=%.2f)", ids[j], closestDock, x[j], y[j]);
                             tags[ids[j]] = tmp;
                         }
                     }
-                    else {
-                        int closestDock = mov.closest_dock(x[j], y[j]);
-                        tmp.dock = closestDock;
-                        ROS_INFO("Adding new AprilTag %u at dock %u (x=%.2f, y=%.2f)", ids[j], closestDock, x[j], y[j]);
-                        tags[ids[j]] = tmp;
-                    }
                 }
             }
+
+            if(i==3) continue;
+
+            ROS_INFO("Object detection results:");
+            ROS_INFO("Table (AprilTag 10) found at x=%f y=%f from dock %u", table_tag.x, table_tag.y, table_tag.dock);
+            for(auto t : tags) 
+                ROS_INFO("AprilTag %u detected at x=%f y=%f from dock %u", t.second.id, t.second.x, t.second.y, t.second.dock);
+
+            // add_reference_collisions(table_tag.x, table_tag.y, coeffs[0], coeffs[1]);
+
+            assignment2::apriltag_detect ad_srv_coll;
+            ad_srv_coll.request.create_collisions = true;
+            std::vector<int> ids;
+            std::vector<float> x, y, z, yaw;
+            for(auto tag : tags) {
+                ids.push_back(tag.second.id);
+                x.push_back(tag.second.x);
+                y.push_back(tag.second.y);
+                z.push_back(tag.second.z);
+                yaw.push_back(tag.second.yaw);
+            }
+            ad_srv_coll.request.ids = ids;
+            ad_srv_coll.request.x = x;
+            ad_srv_coll.request.y = y;
+            ad_srv_coll.request.z = z;
+            ad_srv_coll.request.yaw = yaw;
+            ROS_INFO("Creating collision objects from detections.");
+            if(ad_client.call(ad_srv_coll))
+                ROS_INFO("Call to apriltags_detected_service: SUCCESSFUL.");
+
+            to_move.id = -1;
+            for(auto tag : tags)
+                if(to_move.id == -1 && tag.second.dock==i)
+                    to_move = tag.second;
+            mov.fix_pos();
+            if(to_move.id != -1) {
+                ROS_INFO("Starting the object placing routine.");
+                put_down_routine(to_move, table_tag, put_objs, coeffs[0], coeffs[1], mov);
+                tags.erase(to_move.id);
+            }
+            else 
+                ROS_INFO("No object left to move from dock %u.", i);
         }  
+        while(to_move.id != -1);
     }
-
-    ROS_INFO("Object detection results:");
-    ROS_INFO("Table (AprilTag 10) found at x=%f y=%f from dock %u", table_tag.x, table_tag.y, table_tag.dock);
-    for(auto t : tags) 
-        ROS_INFO("AprilTag %u detected at x=%f y=%f from dock %u", t.second.id, t.second.x, t.second.y, t.second.dock);
-
-    // add_reference_collisions(table_tag.x, table_tag.y, coeffs[0], coeffs[1]);
-
-    assignment2::apriltag_detect ad_srv;
-    ad_srv.request.create_collisions = true;
-    std::vector<int> ids;
-    std::vector<float> x, y, z, yaw;
-    for(auto tag : tags) {
-        ids.push_back(tag.second.id);
-        x.push_back(tag.second.x);
-        y.push_back(tag.second.y);
-        z.push_back(tag.second.z);
-        yaw.push_back(tag.second.yaw);
-    }
-    ad_srv.request.ids = ids;
-    ad_srv.request.x = x;
-    ad_srv.request.y = y;
-    ad_srv.request.z = z;
-    ad_srv.request.yaw = yaw;
-    ROS_INFO("Creating collision objects from detections.");
-    if(ad_client.call(ad_srv))
-        ROS_INFO("Call to apriltags_detected_service: SUCCESSFUL.");
-
-    ROS_INFO("Starting the object placing routine.");
-    for(auto tag : tags){
-	    if(tag.second.dock==4)
-		    dock4.push_back(tag.second);
-	    else if(tag.second.dock==5)
-		    dock5.push_back(tag.second);
-	    else if(tag.second.dock==6)
-		    dock6.push_back(tag.second);
-    }
-    int put_objs = 0;
-    mov.fix_pos();
-    put_down_routine(dock4, table_tag, put_objs, coeffs[0], coeffs[1], mov);
 
     // for(int i=5; i>=1; i--) {
     //     ROS_INFO("Moving to dock %u.", i);
