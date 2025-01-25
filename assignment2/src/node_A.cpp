2f96b23a90ca493f18d56bea3d235b77669724c0
mattiatoff <mattiatoff@Ubuntu20.myguest.virtualbox.org>
2025-01-25 23:51:42 +0100

    Comments for node A + old file removed

--- assignment2/src/node_A.cpp
+++ assignment2/src/node_A.cpp
@@ -15,6 +15,7 @@
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit_msgs/CollisionObject.h>
 
+// Struct for AprilTags info
 struct apriltag_str{
     float x;
     float y;
@@ -25,9 +26,11 @@ struct apriltag_str{
     int dock;
 };
 
+// Class for Node A operations handling
 class Node_A {
 
     private:
+        // Table properties
         const float TABLE_SIDE = 0.91;
         const float PADDING    = 0.07;
 
@@ -43,6 +46,7 @@ class Node_A {
         std::map<int, apriltag_str> tags_place;
 
     public:
+        // Basic constructor, launches setup routine for the pick_and_place one 
         Node_A() : mov(nh_) {
             // Service client to obtain the parameters of the line on which the objects shall be placed
             ros::ServiceClient coeffs_client = nh_.serviceClient<tiago_iaslab_simulation::Coeffs>("/straight_line_srv");
@@ -75,6 +79,7 @@ class Node_A {
             ad_client = nh_.serviceClient<assignment2::apriltag_detect>("apriltags_detected_service");
         }
 
+        // Compute the object placing coordinates, result depends on the numebr of objects placed so far 
         std::pair<float,float> compute_coord(float start_x, float start_y, float yaw) {
             const float OBJ_DIST = 0.12;
             const float TABLE_DIST = 0.08;
@@ -86,6 +91,7 @@ class Node_A {
             return std::make_pair(start_x +cos(yaw)*(tgt_x) - sin(yaw)*(tgt_y) , start_y + sin(yaw)*(tgt_x) + cos(yaw)*(tgt_y));
         }
 
+        // Refine object placing coordinates using detection on last placed object
         bool refine_coord(std::pair<float, float>& coord, float yaw) {
             ROS_INFO("Refining placing coordinates...");
             const float OBJ_DIST = 0.12;
@@ -112,6 +118,7 @@ class Node_A {
 	        return false;
         }
 
+        // Create collision objects from given data calling Node B service
         void add_collision_objs(std::map<int, apriltag_str> tags) {
             assignment2::apriltag_detect ad_srv_coll;
             ad_srv_coll.request.create_collisions = true;
@@ -140,6 +147,7 @@ class Node_A {
                 ROS_INFO("Call to apriltags_detected_service: SUCCESSFUL.");
         }
 
+        // // Remove collision object relative to given id
         // void remove_collision_obj(int id_picked) {
         //     moveit::planning_interface::PlanningSceneInterface planningSceneInterface;
         //     std::map<std::string, moveit_msgs::CollisionObject> colls = planningSceneInterface.getObjects();
@@ -155,38 +163,41 @@ class Node_A {
         //     planningSceneInterface.applyCollisionObjects(to_remove);
         // }
         
+        // Pick up object with given id and place it in the next placing positino
         void pick_place_routine(int to_pick, apriltag_str table_tag, double turn) {
             actionlib::SimpleActionClient<assignment2::ObjectMoveAction> ac("move_object", true);
             ROS_INFO("Waiting for move_object server to start.");
             ac.waitForServer();
 
+            // Info on the object to pick
             apriltag_str tag = tags_pick[to_pick];
 
-            // PICK UP
+            // PICK UP PHASE
             ROS_INFO("Picking up object with AprilTag %u at x=%f y=%f from dock %u.", tag.id, tag.x, tag.y, tag.dock);
 
+            // Update objects info and create collision objects
             for(auto& tag_placed : tags_place) 
                 tags_pick[tag_placed.first] = tag_placed.second;
             add_collision_objs(tags_pick);
-
+            
+            // Setup goal to pick up object
             assignment2::ObjectMoveGoal goal_pick;
             goal_pick.pick = true;
             goal_pick.tgt_id = tag.id;
             goal_pick.tgt_pose.position.x =  tag.x;
             goal_pick.tgt_pose.position.y =  tag.y;
             goal_pick.tgt_pose.position.z =  tag.z;
-
             tf2::Quaternion qt;
-            if(tag.color == 2) // Green objects
+            if(tag.color == 2) // Different (better) gripper orientation for green objects
                 qt.setRPY(0, M_PI_2, tag.yaw);
             else 
                 qt.setRPY(0, M_PI_2, tag.yaw - M_PI_2);
-
             goal_pick.tgt_pose.orientation.w = qt.w();
             goal_pick.tgt_pose.orientation.x = qt.x();
             goal_pick.tgt_pose.orientation.y = qt.y();
             goal_pick.tgt_pose.orientation.z = qt.z();
 
+            // Send picking goal 
             ROS_INFO("Sending goal to move_object server.");
             ac.sendGoal(goal_pick);
             bool finished_before_timeout = ac.waitForResult(ros::Duration(45.0));
@@ -204,21 +215,24 @@ class Node_A {
             // remove_collision_obj(tag.id);
             tags_pick.erase(tag.id);
             
+            // Compute next ideal placing coordinates
             std::pair<float, float> coords = compute_coord(table_tag.x, table_tag.y, table_tag.yaw);
             float put_down_x = coords.first;
             float put_down_y = coords.second;
 
-            mov.spin(-turn);
+            //PLACE DOWN PHASE
             int closestDock = mov.closest_dock(put_down_x, put_down_y);
-
-            //PLACE DOWN
             ROS_INFO("Placing down object with AprilTag %u from dock %u", tag.id, closestDock);
+
+            // Move to optimal placing dock
+            mov.spin(-turn);
             mov.goAround(closestDock);
 
+            // Obtain real placing coordinates if possible
 	        std::vector<double> turns = {0.0, -M_PI_4/3, 2*M_PI_4/3};
             if(placed_last!=-1){
-                for(auto turn: turns) {
-                    mov.spin(turn);
+                for(auto turn_place: turns) {
+                    mov.spin(turn_place);
                     if(refine_coord(coords, table_tag.yaw)) {
                         put_down_x = coords.first;
                         put_down_y = coords.second;
@@ -226,11 +240,12 @@ class Node_A {
                     }
                 }
             }
-
-            add_collision_objs(tags_pick);
-
             ROS_INFO("Placing coordinates: x=%f y=%f", put_down_x, put_down_y);
+
+            // Reload collision objects
+            add_collision_objs(tags_pick);
             
+            // Setup and send goal for object placing
             assignment2::ObjectMoveGoal goal_place;
             goal_place.pick = false;
             goal_place.tgt_id = tag.id;
@@ -256,6 +271,7 @@ class Node_A {
             else
                 ROS_WARN("Action did not finish before timeout.");
 
+            // Update data with latest placing
             placed_objs++;
             tag.x = put_down_x;
             tag.y = put_down_y;
@@ -264,7 +280,7 @@ class Node_A {
             tags_place[tag.id] = tag;
             placed_last = tag.id;
         }
-
+        // Load data structures with detection info
         void load_detections(assignment2::apriltag_detect& ad_srv, std::vector<int>& ids, std::vector<int>& colors, std::vector<float>& x, std::vector<float>& y, std::vector<float>& z, std::vector<float>& yaw) {
             ids = ad_srv.response.id;
             colors = ad_srv.response.color;
@@ -280,19 +296,23 @@ class Node_A {
             //     }).c_str());
         }
 
+        // Launch the full detect, pick and place routine for all objects of the chosen color
         void launch() {
             apriltag_str table_tag{0,0,0,0,-1,-1,-1};
             apriltag_str to_move{0,0,0,0,-1,-1,-1};
             std::vector<int> ids, colors;
             std::vector<float> x, y, z, yaw;
-
+            // Perform the routine from all 6 docking stations
             for(int i=1; i<=6; i++) {
                 std::vector<double> turns = {0.0, -M_PI_4/3, 2*M_PI_4/3};
+                // Loop on the routine until no object that meets the criteria is found and left to be moved
                 do {
                     ROS_INFO("Moving to dock %u.", i);
+                    // At each dock, change slightly the orientation to ensure no AprilTag is missed
                     for(auto turn: turns) {
                         mov.goAround(i);
                         mov.spin(turn);
+                        // Perform AprilTag detection and load data structures
                         assignment2::apriltag_detect ad_srv;
                         ROS_INFO("Sending request to apriltags_detected_service.");
                         if(ad_client.call(ad_srv)) {
@@ -301,18 +321,20 @@ class Node_A {
                             tags_pick.clear();
                             for(int j=0; j<ids.size(); j++) {
                                 apriltag_str tmp{x[j], y[j], z[j], yaw[j], ids[j], colors[j], i};
+                                // Save separately info on the table tag
                                 if(ids[j]==10){
                                     if(table_tag.dock==-1)
                                         table_tag = tmp;
                                     continue;
                                 }
+                                // Associate each detection with the dock closest to it
                                 int closestDock = mov.closest_dock(x[j], y[j]);
                                 tmp.dock = closestDock;
                                 tags_pick[ids[j]] = tmp;
                                 // ROS_INFO("Adding AprilTag %u to dock %u (x=%.2f, y=%.2f)", ids[j], closestDock, x[j], y[j]);
                             }
                         }
-
+                        // Detection results printing
                         if(!ids.empty()) {
                             ROS_INFO("Object detection results:");
                             if(table_tag.dock!=-1)
@@ -322,7 +344,7 @@ class Node_A {
                         }
                         else    
                             ROS_INFO("No AprilTags found.");
-
+                        // Check if a detection meets the conditions, if there i start the pick and place routine for it
                         to_move.id = -1;
                         for(auto tag : tags_pick)
                             if(to_move.id == -1 && tag.second.dock==i && tag.second.color==color_to_pick)
@@ -342,6 +364,8 @@ class Node_A {
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "node_A");
+
+    // Set up the system and launch the detect, pick and place routine
     Node_A node;
     node.launch();
 
